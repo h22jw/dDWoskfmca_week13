6561b868582b429c6243c1e8227ae65fd37218b6
h22jw <jwata@hawaii.edu>
2025-04-20 22:22:20 -1000

    second commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { greeting } from "./greeting.js";
+import "./dress.sass";
+//import "./styles.css"
+
+console.log(greeting);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+        },
+    ]
+  }
+};
